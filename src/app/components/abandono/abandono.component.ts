7f249f841e5c174584984026793b7fcfc74d9b05
Gustav0-Silva <g.silva.1596@gmail.com>
2025-05-14 16:52:17 -0300

    Adição de página Abandono e Voluntariado

--- src/app/components/abandono/abandono.component.ts
+++ src/app/components/abandono/abandono.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AbandonoComponent {
 
+  animarBtn(event: MouseEvent): void {
+    const btn = event.target as HTMLElement;
+    btn.style.backgroundColor = '#f8cbd2';
+    btn.style.color = 'white';
+  }
+
+  resetarBtn(event: MouseEvent): void {
+    const btn = event.target as HTMLElement;
+    btn.style.backgroundColor = 'white';
+    btn.style.color = '#444';
+  }
+
+  abrirFormulario(){
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSejZg1RKVMU50ynMHZki1P466IJ53E9OL_JzxU_iK9lkfjYWw/viewform?usp=header', '_blank');
+  }
 }
--- src/app/components/voluntariado/voluntariado.component.ts
+++ src/app/components/voluntariado/voluntariado.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class VoluntariadoComponent {
 
+  animarBtn(event: MouseEvent): void {
+    const btn = event.target as HTMLElement;
+    btn.style.backgroundColor = '#f8cbd2';
+    btn.style.color = 'white';
+  }
+
+  resetarBtn(event: MouseEvent): void {
+    const btn = event.target as HTMLElement;
+    btn.style.backgroundColor = 'white';
+    btn.style.color = '#444';
+  }
+
+  abrirFormulario(){
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSdw8hrSQLxKiOWbXdXMsCepOPiFhZnegD68S4pi_YRwqwmHwQ/viewform', '_blank');
+  }
+
 }
